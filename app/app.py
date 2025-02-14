f72050f63846b4e2a297f30f7ee73fc9240fa6fc
Ana Saied <anassid07@gmail.com>
2025-02-13 20:17:06 -0500

    Primera fase aplicacion web

--- app/app.py
+++ app/app.py
@@ -1,1 +1,12 @@
-# testing
+from flask import Flask, render_template
+import db 
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return '<h1> Hola Mundo </h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='8000')
